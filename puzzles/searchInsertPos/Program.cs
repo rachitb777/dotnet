4796852c289df88419471d7a00a5651e6b8f2469
Rachit Bajpai <rachitb@live.com>
2021-03-18 11:28:03 +0530

    Search insert position puzzle

---
+++ puzzles/searchInsertPos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace searchInsertPos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
