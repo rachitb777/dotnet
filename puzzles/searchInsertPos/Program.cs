a033cb906d31afeab3d94610a272ae478ac6636c
Rachit Bajpai <rachitb@live.com>
2021-03-18 12:11:17 +0530

    Search for insert position solution

--- puzzles/searchInsertPos/Program.cs
+++ puzzles/searchInsertPos/Program.cs
@@ -6,7 +6,48 @@ namespace searchInsertPos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sol = new Solution();
+            int []nums = {1, 3};
+            Console.WriteLine(sol.SearchInsert(nums, 0));
+        }
+    }
+
+    public class Solution {
+        public int SearchInsert(int[] nums, int target) 
+        {
+            int start = 0;
+            int end = nums.Length - 1;
+            var pos = -1;
+            while(start < end)
+            {
+                int mid = (end + start) / 2;
+                if (nums[mid] == target)
+                {
+                    pos = mid;
+                    break;
+                }
+                else if (nums[mid] < target) 
+                {
+                    start = mid + 1;
+                }
+                else 
+                {
+                    end = mid - 1;
+                }
+            }
+            if (start >= end)
+            {
+                if (target <= nums[start])
+                {
+                    pos = start;
+                }
+                else
+                {
+                    pos = start + 1;
+                }
+            }
+
+            return pos;
         }
     }
 }
